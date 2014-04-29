b1dc898d4dc09e2dff3c1146628cdeee9fab8041
leahsteinberg <leahsteinberg1@gmail.com>
2014-04-29 15:41:56 -0400

    removing unneccessary

--- public/javascripts/key.js
+++
@@ -1,9 +0,0 @@
-var consumerKey = "bASuZcWMkUaRe4iMsSNV1eQw7";
-var consumerToken = "Ly9apdgiM9lZWpuCSroE3vVTIkZZg7I65uDCM3gnczlCR1zxkp";
-var accessToken = "57490723-9XylLaLV6k2u8Iap95c9AzF4w5y0IAuXESAKgzwIb";
-var accessSecret = "vzEHzjREXNWuEkIx9lvQjzKIsMZs3I8FjaSROgd6VKOBG";
-
-module.exports.consumerKey = consumerKey;
-module.exports.consumerToken = consumerToken;
-module.exports.accessToken = accessToken;
-module.exports.accessSecret = accessSecret;

6d4b24a1c2b4847fe5ccd2397231e61a6a9486b0
leahsteinberg <leahsteinberg1@gmail.com>
2014-04-29 14:09:40 -0400

    url

--- server/index.js
+++ server/index.js
@@ -15,8 +15,6 @@ var twitter = new twitterAPI({
 
 var OAuth = require('oauth').OAuth;
 
-
-
 var oa = new OAuth(
 	"https://api.twitter.com/oauth/request_token",
 	"https://api.twitter.com/oauth/access_token",
